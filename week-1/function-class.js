4408b5063b50919cd492d2d3adccb0afc9a16f40
Richard Krasso <rkrasso@bellevue.edu>
2019-02-28 17:01:07 -0600

    Code cleanup.

--- week-1/function-class.js
+++ week-1/function-class.js
@@ -1,12 +1,17 @@
 /*
 ============================================
-; Title:  class.js
+; Title:  function-class.js
 ; Author: Professor Krasso
 ; Date:   28 February 2019
-; Description: Refresher on JavaScript classes
+; Description: Refresher on function classes
 ;===========================================
 */
 
+/**
+ * Description: Function class refresher
+ * @param bedrooms
+ * @constructor
+ */
 function House(bedrooms) {
   this.bedrooms = bedrooms;
   this.color = "White";
@@ -24,24 +29,8 @@ function House(bedrooms) {
   };
 }
 
-var house = new House(5);
+// Instantiate a new House class and pass-in the number of bedrooms as an argument
+const house = new House(5);
 
-console.log(house.getDetails());
-
-var house = {
-  bedrooms: 4,
-  bathrooms: 2,
-  color: "White",
-  getDetails: function() {
-    return (
-      this.bedrooms +
-      " bedroom, " +
-      this.bathrooms +
-      " bath " +
-      this.color +
-      " house."
-    );
-  }
-};
-
+// Output the results to the console debugger window.
 console.log(house.getDetails());
--- week-1/interface.js
+++ week-1/interface.js
@@ -8,47 +8,82 @@
 ;===========================================
 */
 
+/**
+ * Description: Album function class
+ * @param title
+ * @param composer
+ * @constructor
+ */
 function Album(title, composer) {
   this.title = title;
   this.composer = composer;
 }
 
+/**
+ * Description: Prototype for adding Albums to the shopping cart
+ */
 Album.prototype.purchase = function() {
-  console.log("Album added to shopping cart!");
+  console.log('Album added to shopping cart!');
 };
 
+/**
+ * Description: CD function class
+ * @param title
+ * @param composer
+ * @param price
+ * @constructor
+ */
 function Cd(title, composer, price) {
   this.title = title;
   this.composer = composer;
   this.price = price;
 }
 
+/**
+ * Description: Prototype for adding Cd's to the shopping cart
+ */
 Cd.prototype.purchase = function() {
-  console.log("CD added to shopping cart!");
+  console.log('CD added to shopping cart!');
 };
 
+/**
+ * Description: DVD function class
+ * @param title
+ * @param price
+ * @param description
+ * @constructor
+ */
 function Dvd(title, price, description) {
   this.title = title;
   this.price = price;
   this.description = description;
 }
 
+/**
+ * Description: Prototype for adding Dvd's to the shopping cart
+ */
 Dvd.prototype.purchase = function() {
-  console.log("DVD added to shopping cart!");
+  console.log('DVD added to shopping cart!');
 };
 
-var uriCaine = new Album("Uri Caine", "Primal Light");
-var blakeShelton = new Cd("If I'm Honest", "Blake Shelton", 14.99);
-var tron = new Dvd("TRON-LEGACY", 9.96, "Action/Adventure");
+// Object declarations
+let uriCaine = new Album('Uri Caine', 'Primal Light');
+let blakeShelton = new Cd("If I'm Honest", 'Blake Shelton', 14.99);
+let tron = new Dvd('TRON-LEGACY', 9.96, 'Action/Adventure');
 
-var purchased = [];
+// Array to hold all three classes (Album, Cd, and Dvd)
+let purchased = [];
 
+/**
+ * Description: Receives a class an an argument, calls the purchase prototype, and adds the class to the purchased array.
+ * @param item
+ */
 function purchaseIt(item) {
   item.purchase();
-
   purchased.push(item);
 }
 
+// Calls the purchaseIt function passing-in the objects defined in lines 70-72
 purchaseIt(uriCaine);
 purchaseIt(blakeShelton);
 purchaseIt(tron);
@@ -57,6 +92,8 @@ purchaseIt(tron);
 console.log(
   "\n-- The following items have been added to your shopping cart --"
 );
-for (var k = 0; k < purchased.length; k++) {
+
+// Loop over the array and output the results.
+for (let k = 0; k < purchased.length; k++) {
   console.log(purchased[k].constructor.name + ": " + purchased[k].title);
 }
